1d50acaef2618f7af1d5f54d2b7de4a359e18f57
jayeshdeshmukh18 <deshmukhjayesh507@gmail.com>
2022-09-20 19:38:32 +0530

    prime or not

---
+++ prime.js
@@ -0,0 +1,14 @@
+for (let i=1;i<=15;i++){
+    let count=0;
+    for (let j=1;j<=i;j++){
+      if(i%j==0){
+        count++;
+      }
+    }
+  
+    if(count==2){
+      console.log(i,"prime");
+    }else {
+      console.log(i,"Not a Prime")
+    }
+  }
